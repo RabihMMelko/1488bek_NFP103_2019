72d3e9782d8c71c5933cc418f263c814605224ff
Rabih Melko <44175840+RabihMMelko@users.noreply.github.com>
2019-08-02 22:31:56 +0300

    Update ReadThread.java

--- ReadThread.java
+++ ReadThread.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ACCOV2019;
 
 /**
@@ -16,19 +15,20 @@ import java.net.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
- 
+
 /**
  * Thread lecteur du serveur
  */
 public class ReadThread extends Thread {
+
     private BufferedReader reader;
     private Socket socket;
     private Client client;
- 
+
     public ReadThread(Socket socket, Client client) {
         this.socket = socket;
         this.client = client;
- 
+
         try {
             InputStream input = socket.getInputStream();
             reader = new BufferedReader(new InputStreamReader(input));
@@ -37,27 +37,36 @@ public class ReadThread extends Thread {
             ex.printStackTrace();
         }
     }
- 
+
     public void run() {
-        
+
         while (true) {
             try {
-                
-                
-                String response = reader.readLine();
-                System.out.println("\n" + response);
- 
-                // prints the username after displaying the server's message
-                if (client.getUserName() != null) {
-                    DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-                    Date date = new Date();
-                    System.out.print("["+dateFormat.format(date)+"][" + client.getUserName() + "]: ");
+                if (!socket.isClosed()) {
+                    String response = reader.readLine();
+                    if (response.equals("null")) {
+                        try {
+                            this.socket.close();
+                        } catch (Exception e) {
+                            System.out.println("Crash");
+                        }
+                        System.exit(0);
+                    } else {
+                        System.out.println("\n" + response);
+
+                        // prints the username after displaying the server's message
+                        if (client.getUserName() != null) {
+                            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                            Date date = new Date();
+                            System.out.println("[" + dateFormat.format(date) + "][" + client.getUserName() + "]: ");
+                        }
+                    }
                 }
-            } catch (IOException ex) {
-                System.out.println("Erreur lecture du server: " + ex.getMessage());
-                ex.printStackTrace();
-                break;
+            } catch (Exception ex) {
+
+                System.exit(0);
             }
+
         }
     }
 }
